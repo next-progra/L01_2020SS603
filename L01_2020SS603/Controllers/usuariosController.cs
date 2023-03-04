804cff76d3797c71ede590e5d0b37685834b8969
Estudiante <Estudiante@B45PC18>
2023-03-04 11:37:19 -0600

    Agregar archivos de proyecto.

---
+++ L01_2020SS603/Controllers/usuariosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using L01_2020SS603.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01_2020SS603.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        private readonly usuariosController _usuariosContexto;
+
+        public usuariosController(usuariosController usuariosContexto)
+        {
+            _usuariosContexto = usuariosContexto;
+        }
+
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+
+            List<usuarios> listadoUsuarios = (from e in _usuariosContexto.usuarios
+                                              select e).toList();
+
+            if (listadoUsuarios.Count() = 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoUsuarios);
+
+        }
+    }
+
+    
+}
